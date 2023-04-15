7ca14ccf9e58aac0b907f453ff42af89dd26b413
miku1001 <pauloted.22@gmail.com>
2023-04-16 02:20:50 +0800

    Pseudocodes

---
+++ vigenere.py
@@ -0,0 +1,12 @@
+# Create a program that will encrypt given text through Vigenère Cipher
+
+# Import module for loading bar
+# Create welcome greeting
+# Loading Bar
+# Create character dictionary
+# Create a list where you'll put the character
+# Gather the equivalent number of the letter based on dictionary for key and message
+# add the number gathered from message and key
+# Convert the gathered numbers into string and insert it in list
+# Let user input it the data(uppercase, no space)
+# Let the user continue the process or stop
