8661a5ef9aa9f52e213153a271c9ae8a32739b99
miku1001 <pauloted.22@gmail.com>
2023-04-16 02:33:58 +0800

    Character dictionary

--- vigenere.py
+++ vigenere.py
@@ -17,6 +17,10 @@ print(f"Good day {name}\U0001F929, encrypt your message here!\U0001F447")
 print("-" * 50)
 
 # Create character dictionary
+dict_char = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11,
+             'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22,
+             'X': 23, 'Y': 24, 'Z': 25}
+
 # Create a list where you'll put the character
 # Gather the equivalent number of the letter based on dictionary for key and message
 # add the number gathered from message and key
